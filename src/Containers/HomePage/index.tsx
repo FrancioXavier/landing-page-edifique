3ce28872d9bd17419fcdad74b08c396df3253041
FrancioXavier <francioxaviersilva@gmail.com>
2024-05-13 16:59:36 -0300

    New design and our projects

--- src/Containers/HomePage/index.tsx
+++ src/Containers/HomePage/index.tsx
@@ -1,36 +1,87 @@
-import { theme } from '@/styles/theme';
-import { CarouselSection, ContentHome, HomeSection, TitlePage } from './styled';
+// import { theme } from '@/styles/theme';
+import {
+  Card,
+  CarouselSection,
+  ContentHome,
+  ContentSection,
+  HomeSection,
+  TitlePage,
+} from './styled';
 // import background from '@/config/img/bgHome.svg';
 import { Header } from '@/components/Header';
-import backgroundGeral from '@/config/img/bg-geral.svg';
+import backgroundGeral from '@/config/img/newBg.svg';
 import { EmblaOptionsType } from 'embla-carousel';
 import Carousel from '@/components/Carousel';
+import Image from 'next/image';
+import { FaInstagram } from 'react-icons/fa';
+import { FaFacebookF } from 'react-icons/fa6';
+import { FaTwitter } from 'react-icons/fa';
 
 export default function HomePage() {
   const OPTIONS: EmblaOptionsType = { align: 'start' };
   const SLIDETYPE: string = 'sponsors';
   const slideHistory: string = 'stories';
   return (
-    <div
-      style={{
-        backgroundImage: `url(${backgroundGeral.src})`,
-        backgroundPosition: '',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: theme.colors.black,
-        height: 'fit-content',
-      }}
-    >
+    <div style={{ width: '100%', height: 'fit-content' }}>
+      <Image
+        src={backgroundGeral.src}
+        height={100}
+        width={100}
+        priority
+        alt="teste"
+        style={{
+          width: '100%',
+          height: 'fit-content',
+          zIndex: 0,
+          position: 'absolute',
+        }}
+      />
       <Header />
       <HomeSection>
-        <ContentHome>
-          <h1>
-            <span>EDIFIQUE</span>
-            <br /> AÇÕES
-          </h1>
-
-          <p>Cursinho pré-vestibular popular e programa de extensão da UFCA</p>
+        <ContentHome style={{ zIndex: 1 }}>
+          <h1>BEM VINDO AO EDIFIQUE ACOES</h1>
+          <h2>O melhor cursinho gratuito para o ENEM</h2>
+          <button>inscreva-se gratuitamente</button>
+          <div className="icons">
+            <div className="iconContainer">
+              <FaInstagram size={30} />
+            </div>
+            <div className="iconContainer">
+              <FaFacebookF size={30} />
+            </div>
+            <div className="iconContainer">
+              <FaTwitter size={30} />
+            </div>
+          </div>
         </ContentHome>
+      </HomeSection>
+      <HomeSection>
+        <ContentSection>
+          <h1>METODOLOGIA DE ENSINO</h1>
+          <h2>Conheça nossos projetos</h2>
+          <div className="cardSection">
+            <Card>
+              <div className="content">
+                <div className="icon">a</div>
+                <div className="text">
+                  <h3>Simulados para o ENEM</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Laboriosam a quibusdam voluptas? Nihil, blanditiis ullam
+                    repellendus nostrum nemo eos! Accusamus ab iusto quidem
+                    animi dolorem amet iure impedit tenetur esse? Lorem ipsum
+                    dolor, sit amet consectetur adipisicing elit. Harum
+                    veritatis maiores est perspiciatis. Inventore commodi
+                    aliquam, sed dicta laboriosam nihil. Libero officiis
+                    voluptatibus magni maiores ad, iure eos quos consequuntur!
+                  </p>
+                </div>
+              </div>
+            </Card>
+            <Card>a</Card>
+            <Card>a</Card>
+          </div>
+        </ContentSection>
       </HomeSection>
       <CarouselSection>
         <TitlePage>Nossos parceiros</TitlePage>
--- src/Containers/HomePage/styled.ts
+++ src/Containers/HomePage/styled.ts
@@ -4,9 +4,9 @@ export const HomeSection = styled.section`
   height: 100vh;
   width: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  flex-direction: row;
 `;
 
 export const CarouselSection = styled.section`
@@ -35,36 +35,215 @@ export const ContentHome = styled.div`
   text-align: center;
   color: ${({ theme }) => theme.colors.white};
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  span {
-    color: ${({ theme }) => theme.colors.primary};
+  align-items: start;
+  justify-content: start;
+  width: 35vw;
+  height: 40vh;
+  position: absolute;
+  left: 23%; /* por exemplo */
+  top: 50%; /* por exemplo */
+  transform: translate(-50%, -60%);
+  @media only screen and (max-width: 1050px) {
+    top: 40%;
   }
   h1 {
-    font-weight: bold;
-    font-size: 125px;
-    margin-bottom: 3rem;
+    font-size: 30px;
+    margin-bottom: 1.5rem;
 
-    @media only screen and (max-width: 769px) {
-      font-size: 75px;
+    @media only screen and (max-width: 1000px) {
+      font-size: 15px;
+    }
+  }
+  h2 {
+    font-size: 70px;
+    font-weight: bold;
+    text-align: left;
+
+    @media only screen and (max-width: 900px) {
+      font-size: 28px;
+    }
+  }
+
+  button {
+    padding: 10px 40px;
+    font-size: larger;
+    font-weight: bolder;
+    border-radius: 25px;
+    margin-top: 1.5rem;
+    background-color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.white};
+    border: none;
+
+    @media only screen and (max-width: 1000px) {
+      font-size: medium;
+      padding: 10px 30px;
+    }
+  }
+
+  .icons {
+    display: flex;
+    margin-top: 3.5rem;
+    .iconContainer {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      background-color: ${({ theme }) => theme.colors.white};
+      margin-right: 2rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+        color: ${({ theme }) => theme.colors.black};
+      }
+    }
+  }
+  @media only screen and (max-width: 1550px) {
+    h1 {
+      font-size: 20px;
     }
 
-    @media only screen and (max-width: 480px) {
+    h2 {
       font-size: 50px;
     }
   }
-  p {
-    width: 80%;
-    font-size: 25px;
-    font-weight: bold;
 
-    @media only screen and (max-width: 769px) {
+  @media only screen and (max-width: 1300px) {
+    top: 45%;
+
+    h2 {
+      font-size: 35px;
+    }
+  }
+
+  @media only screen and (max-width: 769px) {
+    h1 {
+      font-size: 20px;
+    }
+
+    h2 {
+      font-size: 15px;
+    }
+  }
+
+  @media only screen and (max-width: 480px) {
+    h1 {
       font-size: 15px;
     }
 
-    @media only screen and (max-width: 480px) {
+    h2 {
       font-size: 10px;
+    }
+  }
+`;
+
+export const ContentSection = styled.div`
+  display: flex;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.white};
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  z-index: 1;
+
+  .cardSection {
+    width: 90vw;
+    height: fit-content;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  @media only screen and (max-width: 1050px) {
+    top: 40%;
+  }
+  h1 {
+    font-size: 30px;
+    margin-bottom: 1.5rem;
+
+    @media only screen and (max-width: 1000px) {
+      font-size: 15px;
+    }
+  }
+  h2 {
+    font-size: 70px;
+    font-weight: bold;
+    text-align: left;
+
+    @media only screen and (max-width: 900px) {
+      font-size: 28px;
+    }
+  }
+  @media only screen and (max-width: 1550px) {
+    h1 {
+      font-size: 20px;
+    }
+
+    h2 {
+      font-size: 50px;
+    }
+  }
+
+  @media only screen and (max-width: 1300px) {
+    top: 45%;
+
+    h2 {
+      font-size: 35px;
+    }
+  }
+
+  @media only screen and (max-width: 769px) {
+    h1 {
+      font-size: 20px;
+    }
+
+    h2 {
+      font-size: 15px;
+    }
+  }
+
+  @media only screen and (max-width: 480px) {
+    h1 {
+      font-size: 15px;
+    }
+
+    h2 {
+      font-size: 10px;
+    }
+  }
+`;
+
+export const Card = styled.div`
+  margin-top: 3rem;
+  height: 60vh;
+  width: 25%;
+  background-color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  border-radius: 12px;
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+
+    .icon {
+      margin-top: 2rem;
+      height: 10vh;
+      width: 100%;
+    }
+
+    .text {
+      color: ${({ theme }) => theme.colors.primary};
+      text-align: left;
+      width: 80%;
+      margin-left: 2rem;
+
+      h3 {
+        font-weight: bolder;
+      }
     }
   }
 `;
