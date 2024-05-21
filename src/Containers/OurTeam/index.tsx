7d0f992aff0a8deca622698ff2861a40ee0430f8
FrancioXavier <francioxaviersilva@gmail.com>
2024-05-21 16:03:24 -0300

    ourteam page created

---
+++ src/Containers/OurTeam/index.tsx
@@ -0,0 +1,16 @@
+import { Header } from '@/components/Header';
+import { theme } from '@/styles/theme';
+
+export default function OurTeam() {
+  return (
+    <div
+      style={{
+        height: 'fit-content',
+        width: '100%',
+        background: theme.colors.black,
+      }}
+    >
+      <Header />
+    </div>
+  );
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ export const theme = {
   colors: {
     primary: '#FF5209',
     darkPrimary: '#e94100',
-    black: '#040404',
+    black: '#080404',
     white: '#fff',
     gradient: 'linear-gradient(#040404, #FF5209)',
   },
