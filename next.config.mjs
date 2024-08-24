62a6683f17f055fa72369d789945410e469ede2e
FrancioXavier <francioxaviersilva@gmail.com>
2024-08-24 12:04:48 -0300

    cache test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
