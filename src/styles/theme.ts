b09da1bb019703fd7f6daac98977b763f6c0e30c
FrancioXavier <francioxaviersilva@gmail.com>
2024-08-24 11:48:26 -0300

    contrast test

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
     primary: '#FF5209',
-    darkPrimary: '#e94100',
+    darkPrimary: '#CC3300',
     black: '#080404',
     white: '#fff',
     gradient: 'linear-gradient(#040404, #FF5209)',
